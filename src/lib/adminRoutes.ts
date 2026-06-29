90fff6cf0a0c91ec3266a78ab2ba96b40c1414cd
Sowear <102670787+Sowear@users.noreply.github.com>
2026-06-30 01:56:59 +0300

    1

---
+++ src/lib/adminRoutes.ts
@@ -0,0 +1,16 @@
+/**
+ * Список всех подпутей /admin/* для статической генерации.
+ * Добавляйте сюда новый путь при создании админ-страницы в App.tsx.
+ */
+export const ADMIN_STATIC_PATHS = [
+  "users",
+  "work-examples",
+  "finance-settings",
+  "finance",
+  "editor",
+  "edit",
+  "edit/1",
+  "new",
+  "reset-password",
+  "update-password",
+] as const
